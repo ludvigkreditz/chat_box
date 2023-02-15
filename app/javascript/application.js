fcad04bea74830bb391e63b29d1bf1f5e4a40617
Martin <martin@kreditz.com>
2023-02-15 20:45:26 +0100

    Add auto scrolling to chat window -----

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,10 +5,17 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
+scroll_botton = function() {
+  if ($('#messages').length > 0) {
+    $('messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
 $(document).on('turbo:load', function() {
   $('.ui.dropdown').dropdown();
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  scroll_bottom();
 })
 import "channels"
